a11280255beb01defc7b4e0418eaa5d6e1019a4b
marcushcarter <marcoose408@gmail.com>
2026-02-10 14:34:47 -0500

    02-10

---
+++ unordered_maps.py
@@ -0,0 +1,5 @@
+import random, string
+map = {"walmark":{"beef":12, "celery":4}, "longees":{"beef":19, "celery":5}, "celery":{"beef":53, "celery":11}}
+val = "walmark"
+val2 = "celery"
+print(map[val][val2])
